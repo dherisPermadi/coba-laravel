15c1cf23a04cafb2e14c3d51e46ad96ef249c119
dheris permadi <dheris@sejasa.com>
2023-05-09 16:47:45 +0700

    - Initial Commit - Routes, Model, Collection, and Controller

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Dheris Shatriani Permadi",
+        "email" => "shatrian.permadi@gmail.com",
+        "image" => "image-cat.jpg"
+    ]);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('posts/{slug}', [PostController::class, 'show']);
