fb5b344a4a20115466281e1975c8d5514c0ef117
dheris permadi <dheris@sejasa.com>
2023-05-11 00:38:31 +0700

    Database Seed and Factory Faker

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
   <article>
     <h2>{{ $post->title }}</h2>
-    <p>By. Dheris in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By. <a href="/authors/{{ $post->author->username }}" class='text-decoration-none'>{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class='text-decoration-none'>{{ $post->category->name }}</a></p>
     {!! $post->body !!}
   </article>
 
-  <a href="/posts">Back to Posts</a>
+  <a href="/posts" class='d-block mt-5'>Back to Posts</a>
 @endsection
