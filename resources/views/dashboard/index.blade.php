3bbe214afa51fcbf446039af1419c281562175cb
dheris permadi <dheris@sejasa.com>
2023-05-26 12:34:03 +0700

    Login, Registration and Authentication

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Hi dheris</h1>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,6 +19,29 @@
           <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Category</a>
         </li>
       </ul>
+      <ul class='navbar-nav ms-auto'>
+        @auth
+          <li clas="nav-item dropdown">
+            <a href="#" class="nav-ilnk dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Welcome back, {{ auth()->user()->name }}
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a href="/dashboard" class="dropdown-item"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li>
+                <form action="/logout" method="post">
+                  @csrf
+                  <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                </form>
+              </li>
+            </ul>
+          </li>
+        @else
+          <li class='nav-item'>
+            <a href="/login" class="nav-link"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+          </li>
+        @endauth
+      </ul>
     </div>
   </div>
 </nav>
